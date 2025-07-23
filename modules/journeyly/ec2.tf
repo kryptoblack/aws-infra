e5ff98a13c5dd8c3f23bf7cb3c80637a7cb7b317
Pratik <thisguy@pratikthakare.com>
2025-07-23 13:08:32 +0530

    chore: changed static ami id to dynamic

--- modules/journeyly/ec2.tf
+++ modules/journeyly/ec2.tf
@@ -1,31 +1,32 @@
-# data "aws_ami" "amz_linux" {
-#   most_recent = true
-#
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
-#
-#   filter {
-#     name   = "root-device-type"
-#     values = ["ebs"]
-#   }
-#
-#   filter {
-#     name   = "architecture"
-#     values = ["arm64"]
-#   }
-#
-#   filter {
-#     name   = "name"
-#     values = ["al2023-ami-2023*-arm64"]
-#   }
-#
-#   owners = ["137112412989"] # amazon
-# }
+data "aws_ami" "amz_linux" {
+  most_recent = true
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*-arm64"]
+  }
+
+  owners = ["137112412989"] # amazon
+}
 
 resource "aws_instance" "free" {
-  ami           = "ami-08d019c28ad8d0847"
+  # ami           = "ami-08d019c28ad8d0847"
+  ami           = data.aws_ami.amz_linux.id
   instance_type = "t4g.small"
 
   # network_interface {
