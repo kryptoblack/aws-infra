d642996290d1e3c7d6528b505f4aadfd44369647
Pratik <thisguy@pratikthakare.com>
2025-07-23 07:10:58 +0530

    fix: removed unneeded vars

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "region" {
   default     = "ap-south-1"
 }
 
-variable "budget_subscription_emails" {
-  description = "Email address to subscribe to budget notifications"
-  type        = list(string)
-}
+# variable "budget_subscription_emails" {
+#   description = "Email address to subscribe to budget notifications"
+#   type        = list(string)
+# }
